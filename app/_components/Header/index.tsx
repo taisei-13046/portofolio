e9e02419452195b147ac7885598b4c4ea5572608
安井大晟 <taisei_yasui@yasui-taiseiMacBook-Air.local>
2023-10-10 10:35:35 +0100

    install shadcn/ui and setup dark mode

---
+++ app/_components/Header/index.tsx
@@ -0,0 +1,10 @@
+import { ModeToggle } from "@/components/ModeToggle";
+
+export const Header = () => {
+  return (
+    <nav>
+      <h2>{"ytaisei's Blog"}</h2>
+      <ModeToggle />
+    </nav>
+  );
+};
